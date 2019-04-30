46e1ee79539ecb2027e224ac321bd28823592a27
JayBo7 <jvboinapalli@ucdavis.edu>
2019-04-30 16:26:46 -0700

    display dates from most recent to least recent in datatable

--- client/components/Table.jsx
+++ client/components/Table.jsx
@@ -1,9 +1,17 @@
 import TableEntry from './TableEntry';
 
-const Table = ({ labels, values }) => (
-	<div>
-		{labels.map((date, index) => <TableEntry date={date} value={values[index]} />)}
-	</div>
+const Table = ({ labels, values, currency }) => (
+		<table>
+			<thead>
+				<tr>
+					<th>Date</th> 
+					<th>Value</th>
+				</tr>
+			</thead>
+			<tbody>
+				{labels.map((date, index) => <TableEntry key={index} date={labels[labels.length - index - 1]} value={values[labels.length - index - 1]} currency={currency} />)}
+			</tbody>
+		</table>
 )
 
 export default Table;
